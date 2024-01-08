763f6c213236be4bd634a55112d4035c711622d0
anouar <anouar.ab95@gmail.com>
2024-01-08 16:03:29 +0100

    update icon

--- app/views/pages/Dashbord/Wiki.php
+++ app/views/pages/Dashbord/Wiki.php
@@ -68,11 +68,16 @@ require APPROOT . '/views/inc/sidebar.php';
 
 
                                                 <td>
-                                                    <a class="update_Wiki" type="button" data-bs-toggle="modal"
-                                                        value="<?= $Wiki->getContent() ?>"
+                                                    <a class="update_Wiki btn btn-primary" type="button"
+                                                        data-bs-toggle="modal" value="<?= $Wiki->getContent() ?>"
                                                         data-key="<?= $Wiki->getWikiID() ?>"
                                                         data-bs-target="#updateGenreModal" data-bs-whatever="@mdo">
-                                                        <i class="btn btn-primary far fa-pen"></i>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                            fill="currentColor" class=" bi bi-check"
+                                                            viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
+                                                        </svg>
 
                                                         <a
                                                             href="<?= URLROOT ?>/AdminController/DelletTags?id=<?= $Wiki->getWikiID() ?>"><i
@@ -109,7 +114,7 @@ require APPROOT . '/views/inc/sidebar.php';
                                 <form method="POST" action="<?= URLROOT ?>/AdminController/UpdateTags"
                                     enctype="multipart/form-data">
                                     <div class="mb-3">
-                                        <label for="Wiki" class="col-form-label">Wiki:</label>
+                                        <label for="Wiki" class="col-form-label">Content:</label>
                                         <input id="idTagsWiki" type="hidden" class="form-control" name="id">
                                         <textarea id="Wiki" type="text" class="form-control" name="Wiki"></textarea>
                                     </div>
